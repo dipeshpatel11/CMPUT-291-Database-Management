98522df13c799c6ea809a173c16508e8801cbfe4
Dipesh Patel <117682312+dipeshpatel11@users.noreply.github.com>
2023-05-02 13:50:01 -0600

    Add files via upload

---
+++ MongoDB Norm & Embeded/A4Q1Norm.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import argparse
+
+#to get the port number from command input
+comm_input = argparse.ArgumentParser()
+comm_input.add_argument('port_number', type=int)
+argument = comm_input.parse_args()
+port_number = argument.port_number
+
+
+client = MongoClient('localhost', port_number)
+db = client["A4dbNorm"]
+
+query = db.songwriters.aggregate([
+    {"$project": 
+        {"_id": 1,
+         "songwriter_id": 1,
+         "name": 1,
+        "num_recordings" : {'$size': "$recordings"},
+        }
+    },
+    {
+        "$match": 
+            {"num_recordings": {"$gt":0}}
+    }
+])
+
+for q in query:
+    print(q)
+
